32c4bea67f85b1563af51c731d678bf99b57e578
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-01-01 23:04:21 +0700

    update

--- code/main.sql
+++ code/main.sql
@@ -1,3 +1,5 @@
+-- run ssms.exe in command line
+
 CREATE LOGIN MyLogin WITH password='123456';
 DROP LOGIN MyLogin;
 DROP DATABASE sqltutorials;
@@ -11,22 +13,36 @@ CREATE USER hoangnd for login MyLogin;
 
 DROP TABLE Shippers;
 CREATE TABLE Shippers (
-	ShipperId int NOT NULL IDENTITY PRIMARY KEY,
+	ShipperId int NOT NULL IDENTITY PRIMARY KEY,    
     CompanyName varchar(255),
     Phone varchar(20)
 );
-INSERT INTO Shippers(CompanyName, Phone) VALUES('Federal Shipper', '(555)-123-456');
+INSERT INTO Shippers(CompanyName, Phone) VALUES('Speedy Shipper', '(555)-123-4567');
+INSERT INTO Shippers(CompanyName, Phone) VALUES('Federal Express', '(503)-444-3123');
+INSERT INTO Shippers(CompanyName, Phone) VALUES('Shipper', '(555)-123-7896');
 CREATE TABLE Categories (
 	CategoryId int NOT NULL IDENTITY PRIMARY KEY,
     CategoryName varchar(255),
     Description text
 );
+DELETE FROM Categories;
+INSERT INTO Categories(CategoryName, Description) VALUES('Beverages','Coffees, tea, beers, juice')
+INSERT INTO Categories(CategoryName, Description) VALUES('Seafood','Fish, Shrimp, squid, oyster')
+INSERT INTO Categories(CategoryName, Description) VALUES('Confections','Candies, sweet breads, desserts')
+
 CREATE TABLE Sales (
 	SaleId int IDENTITY PRIMARY KEY,
     FirstName varchar(255),
     LastName varchar(255),
-    HireData date
+    HireDate datetime
 );
-INSERT INTO Sales(FirstName, LastName) VALUES('John', 'Smith');
+DELETE FROM Sales;
+INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('John', 'Smith', CURRENT_TIMESTAMP);
+INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Nancy', 'Robert', '12-24-2017');
+INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Janet', 'King', '11-21-2017');
+INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Michael', 'Davolio', '5-21-2018');
+INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Anna', 'Syanana', '4-15-2018');
+INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Magaret', 'Dodswoth', '3-12-2018');
+INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Robert', 'Valady', '4-15-2018');
 
-
+SELECT * FROM Sales;
