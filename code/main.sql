036fb7c049fb07c9eb0ce50faf5a9f201abee454
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-01-04 17:04:30 +0700

    update

--- code/main.sql
+++ code/main.sql
@@ -11,38 +11,50 @@ RESTORE DATABASE sqltutorials FROM DISK = 'c:\mssql\sqltutorials.bak';
 DROP USER hoangnd;
 CREATE USER hoangnd for login MyLogin;
 
-DROP TABLE Shippers;
-CREATE TABLE Shippers (
-	ShipperId int NOT NULL IDENTITY PRIMARY KEY,    
-    CompanyName varchar(255),
-    Phone varchar(20)
-);
-INSERT INTO Shippers(CompanyName, Phone) VALUES('Speedy Shipper', '(555)-123-4567');
-INSERT INTO Shippers(CompanyName, Phone) VALUES('Federal Express', '(503)-444-3123');
-INSERT INTO Shippers(CompanyName, Phone) VALUES('Shipper', '(555)-123-7896');
-CREATE TABLE Categories (
-	CategoryId int NOT NULL IDENTITY PRIMARY KEY,
-    CategoryName varchar(255),
-    Description text
-);
-DELETE FROM Categories;
-INSERT INTO Categories(CategoryName, Description) VALUES('Beverages','Coffees, tea, beers, juice')
-INSERT INTO Categories(CategoryName, Description) VALUES('Seafood','Fish, Shrimp, squid, oyster')
-INSERT INTO Categories(CategoryName, Description) VALUES('Confections','Candies, sweet breads, desserts')
+DROP TABLE Customers;
 
-CREATE TABLE Sales (
-	SaleId int IDENTITY PRIMARY KEY,
-    FirstName varchar(255),
-    LastName varchar(255),
-    HireDate datetime
+CREATE TABLE Customers (
+	CustomerID int NOT NULL IDENTITY PRIMARY KEY,    
+    CustomerName varchar(300),
+    Address varchar(1000),
+    Phone varchar(20),
+    City varchar(150),
+    Country varchar(200),
 );
-DELETE FROM Sales;
-INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('John', 'Smith', CURRENT_TIMESTAMP);
-INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Nancy', 'Robert', '12-24-2017');
-INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Janet', 'King', '11-21-2017');
-INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Michael', 'Davolio', '5-21-2018');
-INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Anna', 'Syanana', '4-15-2018');
-INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Magaret', 'Dodswoth', '3-12-2018');
-INSERT INTO Sales(FirstName, LastName, HireDate) VALUES('Robert', 'Valady', '4-15-2018');
+INSERT INTO Customers(CustomerName,Address,Phone,City,Country) VALUES('Thomas Hardy', '120 Hanover Sq','0123-456-789','London', 'UK');
+INSERT INTO Customers(CustomerName,Address,Phone,City,Country) VALUES('Ann Devon', '35 King George','0234-458-417', 'Madrid', 'Spain');
+INSERT INTO Customers(CustomerName,Address,Phone,City,Country) VALUES('Maria Larsson', 'Åkergatan 24','0111-456-325', 'Bräcke', 'Sweden');
+INSERT INTO Customers(CustomerName,Address,Phone,City,Country) VALUES('Christina Berglund', 'Berguvsvägen 8','0111-456-325', 'Luleå', 'Sweden');
+INSERT INTO Customers(CustomerName,Address,Phone,City,Country) VALUES('André Fonseca', 'Av. Brasil, 442','0333-142-687', 'Campinas', 'Brazil');
+INSERT INTO Customers(CustomerName,Address,Phone,City,Country) VALUES('Lehmanns Marktstand', 'Magazinweg 7','0222-596-977', 'Frankfurt', 'Germany');
 
-SELECT * FROM Sales;
+SELECT CustomerName, Phone,City, Country FROM Customers;
+SELECT * FROM Customers;
+SELECT DISTINCT(Country) FROM Customers;
+SELECT COUNT(DISTINCT(Country)) FROM Customers;
+SELECT * FROM Customers WHERE Country='Sweden';
+SELECT * FROM Customers WHERE CustomerID BETWEEN 1 AND 3;
+SELECT * FROM Customers WHERE Country='Sweden' AND City='Luleå';
+SELECT * FROM Customers WHERE Country='Germany' OR Country='Spain';
+SELECT * FROM Customers WHERE NOT CustomerID=1;
+SELECT * FROM Customers ORDER BY Country;
+SELECT * FROM Customers ORDER BY Country DESC;
+
+INSERT INTO Customers(CustomerName,Address,Phone,City,Country) VALUES('John Steel', '12 Orchestra Terrace','0222-596-977', 'Walla Walla', NULL);
+SELECT * FROM Customers WHERE Country is NULL;
+SELECT * FROM Customers WHERE Country is NOT NULL;
+
+SELECT * FROM Customers WHERE CustomerID=1;
+UPDATE Customers SET CustomerName = 'Thomas Hardy A', City= 'England' WHERE CustomerID = 1;
+--DELETE FROM Customers WHERE ;
+SELECT TOP 4 * FROM Customers;
+SELECT TOP 50 PERCENT * FROM Customers;
+
+CREATE TABLE Products (
+    ProductID int NOT NULL IDENTITY PRIMARY KEY,    
+    ProductName varchar(400),
+    SupplierID int,
+    CategoryID int,
+    Unit varchar(250),
+    Price float,
+);
